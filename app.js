49056740c141c0e36d04dbf49aac9677e142fe59
sharmishtha21 <159533474+sharmishtha21@users.noreply.github.com>
2025-03-10 15:02:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function addTask() {
+  let input = document.getElementById("input-box");
+  let taskList = document.getElementById("list-container");
+
+  function addTask(){
+    if(inputBox.value ====''{
+      alert("You must write something!");
+    }
+    else{
+      let li = document.createElement("li");
+      li.innerHTML = inputBox.value;
+      listContainer.appendChild(li);
+      let span = document.createElement("span");
+      span.innerHtml = "\u00d7";
+      li.appendChild(span);
+    }
+  )
+  inputBox.value = " ";
